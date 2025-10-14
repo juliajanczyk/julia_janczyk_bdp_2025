ec17b252d39b82a83c952d3d2da774f5a2d8923e
juliajanczyk <147414369+juliajanczyk@users.noreply.github.com>
2025-10-14 09:21:11 +0200

    Update zad3.sql
    Edycja zadania 3

--- 01_setup/zad3.sql
+++ 01_setup/zad3.sql
@@ -17,18 +17,21 @@ CREATE TABLE IF NOT EXISTS księgowość.godziny (
 	FOREIGN KEY (id_pracownika)
 	REFERENCES księgowość.pracownicy(id_pracownika)
 );
+COMMENT ON TABLE księgowość.godziny IS 'Tabela przechowuje informacje o liczbie przepracowanych godzin przez pracowników danego dnia';
 
 CREATE TABLE IF NOT EXISTS  księgowość.pensja (
 	id_pensji int PRIMARY KEY,
 	stanowisko char(200),
 	kwota  money NOT NULL
 );
+COMMENT ON TABLE księgowość.pensja IS 'Tabela zawiera dane o wynagrodzeniach dla danych stanowisk';
 
 CREATE TABLE IF NOT EXISTS księgowość.premia (
 	id_premii int PRIMARY KEY,
 	rodzaj char(200),
 	kwota  money 
 );
+COMMENT ON TABLE księgowość.premia IS 'Tabela zawiera dane o premiach przyznawanych pracownikom';
 
 CREATE TABLE IF NOT EXISTS księgowość.wynagrodzenie (
 	id_wynagrodzenia int PRIMARY KEY,
@@ -50,3 +53,5 @@ CREATE TABLE IF NOT EXISTS księgowość.wynagrodzenie (
 		FOREIGN KEY (id_premii)
 			REFERENCES księgowość.premie(id_premii)
 );
+COMMENT ON TABLE księgowość.wynagrodzenie IS 'Tabela przechowuje dane dotyczące wynagrodzeń pracowników, uwzględniających pensję i premię, przepracowane godziny oraz datę';
+
